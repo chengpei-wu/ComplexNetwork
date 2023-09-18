f5d68532e81e85f6462bfcb7190254481a6367e6
wcphaha <1683168521@qq.com>
2023-09-18 15:46:51 +0800

    add controllability robustness calculation

--- CompelxNetwork/Robustness/SimulatedAttack.py
+++ CompelxNetwork/Robustness/SimulatedAttack.py
@@ -34,12 +34,36 @@ def ConnectivityRobustness(graph, attack='node', strategy='degree'):
     return robustness_curve, np.mean(robustness_curve)
 
 
-# todo: ControllabilityRobustness
-
 def ControllabilityRobustness(graph, attack='node', strategy='degree'):
-    pass
+    G = deepcopy(graph)
+    N = G.number_of_nodes()
+    attack_sequence = Attack(G, attack=attack, strategy=strategy)
+
+    rank_adj = np.linalg.matrix_rank(nx.to_numpy_matrix(G))
+    r_0 = max(1, N - rank_adj) / N
+    robustness_curve = [r_0]
+
+    for i, target in enumerate(attack_sequence):
+        if attack == 'node':
+            G.remove_node(target)
+            rank_adj = np.linalg.matrix_rank(nx.to_numpy_matrix(G))
+            r_i = max(1, (N - i - 1) - rank_adj) / (N - i - 1)
+        elif attack == 'edge':
+            G.remove_edge(*target)
+            rank_adj = np.linalg.matrix_rank(nx.to_numpy_matrix(G))
+            r_i = max(1, N - rank_adj) / N
+        else:
+            raise AttributeError(f'Attack : {attack}, NOT Implemented.')
+        robustness_curve.append(r_i)
+
+    return robustness_curve, np.mean(robustness_curve)
 
 
 # todo: CommunicabilityRobustness
 def CommunicabilityRobustness(graph, attack='node', strategy='degree'):
     pass
+
+
+g = nx.barabasi_albert_graph(100, 4)
+g1 = nx.DiGraph(nx.to_directed(g))
+print(ControllabilityRobustness(g1, attack='node', strategy='degree'))
